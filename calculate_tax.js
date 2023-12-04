23237d5e5d140a1ed74da6c79dfcfa0040dbb5cb
MSerranoK12 <138093990+MSerranoK12@users.noreply.github.com>
2023-12-03 22:49:33 -0800

    Update calculate_tax.js

--- calculate_tax.js
+++ calculate_tax.js
@@ -1,8 +1,6 @@
 "use strict";
 const $ = selector => document.querySelector(selector);
-
 document.addEventListener("DOMContentLoaded", () => {
 	// add event handlers
 	$("#calculate").addEventListener("click", processEntry);
-
 });
